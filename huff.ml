fb8b800accffdd3275ba1990259b5454079eee26
Jeremy Rakotonirina <jeremy.rakotonirina@etu-upsaclay.fr>
2025-01-13 21:14:46 +0100

    Début char freq

--- huff.ml
+++ huff.ml
@@ -2,3 +2,14 @@ let main () = Huffman.decompress "fichier"
 
 
 let () = main ()
+
+let nomfichier = Sys.argv.(1) in
+try
+  let cin = open_in nomfichier in
+  let frequences = Huffman.char_freq cin in
+  close_in cin; 
+  Array.iteri (fun i count ->
+    if count > 0 then Printf.printf "Byte %d: %d occurrences\n" i count
+  ) frequences
+with Sys_error msg ->
+  -1
--- huffman.ml
+++ huffman.ml
@@ -1,2 +1,10 @@
 let decompress _ = failwith "todo"
 let compress _ = failwith "todo"
+
+let char_freq(in_channel):int array=
+  let freq = Array.make 256 0 in
+  try 
+    while true do 
+      let byte= input_byte in_channel in
+      freq.(byte) <- freq.(byte) + 1
+  with End_of_file -> freq
