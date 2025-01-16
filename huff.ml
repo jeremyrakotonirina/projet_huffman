4f07ae52bf5b3d21e1562b4c1807cacaf9258285
Jeremy Rakotonirina <jeremy.rakotonirina@etu-upsaclay.fr>
2025-01-16 20:15:07 +0100

    Améliroation char_freq et adaptatioin huff.ml

--- huff.ml
+++ huff.ml
@@ -3,13 +3,14 @@ let main () = Huffman.decompress "fichier"
 
 let () = main ()
 
-let nomfichier = Sys.argv.(1) in
-try
-  let cin = open_in nomfichier in
-  let frequences = Huffman.char_freq cin in
-  close_in cin; 
-  Array.iteri (fun i count ->
-    if count > 0 then Printf.printf "Byte %d: %d occurrences\n" i count
-  ) frequences
-with Sys_error msg ->
-  -1
+let main()=
+  let nomfichier = Sys.argv.(1) in
+  try
+    let cin = open_in nomfichier in
+    let frequences = Huffman.char_freq cin in
+    close_in cin; 
+    Array.iteri (fun i freq -> (*itère sur les indices et les valeurs*)
+      if freq > 0 then Printf.printf "Byte %d: %d occurrences\n" i freq
+    ) frequences
+  with Sys_error msg ->
+    Printf.printf "Erreur"
--- huffman.ml
+++ huffman.ml
@@ -1,10 +1,12 @@
-let decompress _ = failwith "todo"
-let compress _ = failwith "todo"
+(*let decompress _ = failwith "todo" *)
+(*let compress _ = failwith "todo"*)
 
-let char_freq(in_channel):int array=
+let char_freq(channel:in_channel):int array=
   let freq = Array.make 256 0 in
   try 
-    while true do 
-      let byte= input_byte in_channel in
+    while true do
+      let byte= input_byte channel in
       freq.(byte) <- freq.(byte) + 1
+    done;
+    freq
   with End_of_file -> freq
