70f7f188bb380c00087b91a2724a47b9bce0e175
Jeremy Rakotonirina <jeremy.rakotonirina@etu-upsaclay.fr>
2025-01-17 17:45:14 +0100

    changement stucture file de priorité tas de binaire en tableau

--- heap.ml
+++ heap.ml
@@ -1,32 +1,67 @@
 
-type 'a t = (*tas binaire minimum pour la file de priorité*)
-    |Empty
-    |Node of 'a * 'a t * 'a t
+type 'a t = {
+  data : 'a array;  (* Tableau  pour stocker les éléments *)
+  mutable taille : int;  (* Taille actuelle du tas *)
+}
 
-let empty = Empty
+let empty = {
+    data = Array.make 256 0; (*car 256 bites*)
+    taille = 0;
+  }
 
-let rec add e tas =
-    match tas with
-    | Empty -> Node (e, Empty, Empty)
-    | Node (y, gauche, droite) -> 
-      if compare e y <= 0 then (*Si e<=y*)
-        Node (e, add y gauche, droite)  
-      else
-        Node (y, add e gauche, droite)
+let parent i = (i - 1) / 2
+
+let enfant_gauche i = 2 * i + 1
+
+let enfant_droite i = 2 * i + 2
+
+(* Échanger deux éléments dans le tableau *)
+let echange arr i j =
+  let temp = arr.(i) in
+  arr.(i) <- arr.(j);
+  arr.(j) <- temp
+
+let add heap x = (*algorithme ajout élément tas binaire*)
+    heap.data.(heap.taille) <- x;
+    let rec remonter i =
+      if i > 0 then
+        let p = parent i in
+        if (heap.data.(i) < heap.data.(p)) then begin
+          echange heap.data i p;
+          remonter p
+        end
+    in
+    remonter heap.taille;
+    heap.taille <- heap.taille + 1
 
 let is_singleton heap =
-    match heap with
-    | Node (_, Empty, Empty) -> true
-    | _ -> false
+    heap.taille==1
 
 let is_empty heap =
-    match heap with
-    | Empty -> true
-    | _ -> false
+    heap.taille==0
 
 let find_min heap =
-    match heap with
-    | Empty -> failwith "Le tas est vide"
-    | Node (x, _, _) -> x
+    if heap.taille = 0 then failwith "Le tas est vide";
+    heap.data.(0)
 
-let remove_min _ = failwith "todo"
+let remove_min heap = (*algorithme suppression élément tas binaire*)
+    if heap.taille = 0 then failwith "Le tas est vide";
+    let min_elem = heap.data.(0) in
+    heap.taille <- heap.taille - 1;
+    heap.data.(0) <- heap.data.(heap.taille);
+    let rec descendre i =
+        let l = enfant_gauche i in
+        let r = enfant_droite i in
+        let smallest =
+            if l < heap.taille && heap.data.(l) < heap.data.(i) then l else i
+        in
+        let smallest =
+            if r < heap.taille && heap.data.(r) < heap.data.(smallest) then r else smallest
+        in
+        if smallest != i then begin
+            echange heap.data i smallest;
+            descendre smallest
+        end
+    in
+    descendre 0;
+    min_elem
