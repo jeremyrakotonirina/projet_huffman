981db91cee3420a541104410716397b30481519a
Jeremy Rakotonirina <jeremy.rakotonirina@etu-upsaclay.fr>
2025-01-18 12:33:06 +0100

    Ajustements et modification heap.mli

--- heap.ml
+++ heap.ml
@@ -1,13 +1,15 @@
 
-type 'a t = {
-  data : 'a array;  (* Tableau  pour stocker les éléments *)
-  mutable taille : int;  (* Taille actuelle du tas *)
+type element = int * char
+
+type t = {
+  data : element array;  (* Tableau contenant des couples (freq, char) *)
+  mutable taille : int;    (* Taille actuelle du tas *)
 }
 
 let empty = {
-    data = Array.make 256 0; (*car 256 bites*)
+    data = Array.make 256 (0,'\000'); (*car 256 bites et on initialise avec le caractère nul*)
     taille = 0;
-  }
+}
 
 let parent i = (i - 1) / 2
 
@@ -16,23 +18,25 @@ let enfant_gauche i = 2 * i + 1
 let enfant_droite i = 2 * i + 2
 
 (* Échanger deux éléments dans le tableau *)
-let echange arr i j =
-  let temp = arr.(i) in
-  arr.(i) <- arr.(j);
-  arr.(j) <- temp
+let echange heap i j =
+  let temp = heap.(i) in
+  heap.(i) <- heap.(j);
+  heap.(j) <- temp
 
-let add heap x = (*algorithme ajout élément tas binaire*)
-    heap.data.(heap.taille) <- x;
+let add elt heap  = (*algorithme ajout élément tas binaire*)
+    heap.data.(heap.taille) <- elt;
     let rec remonter i =
       if i > 0 then
         let p = parent i in
-        if (heap.data.(i) < heap.data.(p)) then begin
+        if (fst heap.data.(i) < fst heap.data.(p)) then begin
           echange heap.data i p;
           remonter p
         end
     in
     remonter heap.taille;
-    heap.taille <- heap.taille + 1
+    heap.taille <- heap.taille + 1;
+    heap
+    
 
 let is_singleton heap =
     heap.taille==1
@@ -45,10 +49,11 @@ let find_min heap =
     heap.data.(0)
 
 let indice_petit heap i j=
-    if j < heap.taille && heap.data.(j) < heap.data.(i) then j else i
+    if j < heap.taille && fst heap.data.(j) < fst heap.data.(i) then j else i
 
 let remove_min heap = (*algorithme suppression élément tas binaire*)
     if heap.taille = 0 then failwith "Le tas est vide";
+    let min_elem = heap.data.(0) in
     heap.taille <- heap.taille - 1;
     heap.data.(0) <- heap.data.(heap.taille); (*car dernier élément du tablea à taille-1*)
     let rec descendre i =
@@ -62,3 +67,4 @@ let remove_min heap = (*algorithme suppression élément tas binaire*)
         end
     in
     descendre 0;
+    (min_elem, heap)
--- heap.mli
+++ heap.mli
@@ -1,24 +1,26 @@
-type 'a t 
+type element = int * char
+
+type t
 
 (** The type of heaps. Elements are ordered using generic comparison.
 *)
 
-val empty : 'a t
+val empty : t
 (** [empty] is the empty heap. *)
 
-val add : 'a -> 'a t -> 'a t
+val add : element -> t -> t
 (** [add e h] add element [e] to [h]. *)
 
-val find_min : 'a t -> 'a
+val find_min : t -> element
 (** [find_min h] returns the smallest elements of [h] w.r.t to 
     the generic comparison [<] *)
 
-val remove_min : 'a t -> 'a * 'a t
+val remove_min : t -> element * t
 (** [remove_min h] returns the pair of the smallest elements of [h] w.r.t to 
     the generic comparison [<] and [h] where that element has been removed. *)
 
-val is_singleton : 'a t -> bool
+val is_singleton : t -> bool
 (** [is_singleton h] returns [true] if [h] contains one element *)
 
-val is_empty : 'a t -> bool
+val is_empty : t -> bool
 (** [is_empty h] returns [true] if [h] contains zero element *)
